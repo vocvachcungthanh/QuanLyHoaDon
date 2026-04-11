06c17333f4603e63cf88a2d66d728ba01d6d954a
vocvachcungthanh <vocvachucngthanh@gmail.com>
2026-04-11 16:58:40 +0700

    feat: upload code

---
+++ env.js
@@ -0,0 +1,5 @@
+// Điền thông tin Supabase của bạn vào đây
+// File này đã được thêm vào .gitignore — KHÔNG commit file này lên git
+const SUPABASE_URL = "https://wlaswjycflqrtcwxnrhv.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndsYXN3anljZmxxcnRjd3hucmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQ0ODYsImV4cCI6MjA5MTQwMDQ4Nn0.1q8U0MtIzfu3qRw2mU_huFpFcpxanG7uoO16EaEhL5A";
